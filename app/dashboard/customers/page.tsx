af27c9644ce69a4a190e5ecaf80782ea7d1add19
thien.nguyen <idunno.aiii@gmail.com>
2025-08-09 14:10:20 +0700

    feat: navigation

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Page() {
+  return <div>Customer page</div>
+}
+
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>Invoice Page</div>
+  )
+}
+
+export default Page
